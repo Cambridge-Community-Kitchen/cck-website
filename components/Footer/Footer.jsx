8cdb95eabd0b4977e4177dc1988ed405ac749409
DinerIsmail <ismail.diner@gmail.com>
2021-04-15 14:50:02 +0100

    Remove react import

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -12,6 +12,46 @@ import {
 } from '@chakra-ui/react';
 import { FaInstagram, FaTwitter, FaFacebook } from 'react-icons/fa';
 
+const Footer = () => {
+	return (
+		<Box
+			bg={useColorModeValue('gray.50', 'gray.900')}
+			color={useColorModeValue('gray.700', 'gray.200')}
+		>
+			<Container
+				as={Stack}
+				maxW={'6xl'}
+				py={4}
+				direction={{ base: 'column', md: 'row' }}
+				spacing={4}
+				justify={{ base: 'center', md: 'space-between' }}
+				align={{ base: 'center', md: 'center' }}
+			>
+				<Logo />
+				<Stack direction="row" spacing={6}>
+					<SocialButton
+						label="Instagram"
+						href="https://www.instagram.com/cambridgecommunitykitchen/"
+					>
+						<FaInstagram />
+					</SocialButton>
+					<SocialButton label="Twitter" href="https://twitter.com/camcommunity">
+						<FaTwitter />
+					</SocialButton>
+					<SocialButton
+						label="Facebook"
+						href="https://www.facebook.com/cambridgecommunitykitchen"
+					>
+						<FaFacebook />
+					</SocialButton>
+				</Stack>
+			</Container>
+		</Box>
+	);
+};
+
+export default Footer;
+
 const Logo = (props) => {
 	return (
 		<Flex>
@@ -52,43 +92,3 @@ const SocialButton = ({ children, label, href }) => {
 		</chakra.button>
 	);
 };
-
-const Footer = () => {
-	return (
-		<Box
-			bg={useColorModeValue('gray.50', 'gray.900')}
-			color={useColorModeValue('gray.700', 'gray.200')}
-		>
-			<Container
-				as={Stack}
-				maxW={'6xl'}
-				py={4}
-				direction={{ base: 'column', md: 'row' }}
-				spacing={4}
-				justify={{ base: 'center', md: 'space-between' }}
-				align={{ base: 'center', md: 'center' }}
-			>
-				<Logo />
-				<Stack direction="row" spacing={6}>
-					<SocialButton
-						label="Instagram"
-						href="https://www.instagram.com/cambridgecommunitykitchen/"
-					>
-						<FaInstagram />
-					</SocialButton>
-					<SocialButton label="Twitter" href="https://twitter.com/camcommunity">
-						<FaTwitter />
-					</SocialButton>
-					<SocialButton
-						label="Facebook"
-						href="https://www.facebook.com/cambridgecommunitykitchen"
-					>
-						<FaFacebook />
-					</SocialButton>
-				</Stack>
-			</Container>
-		</Box>
-	);
-};
-
-export default Footer;
