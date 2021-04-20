535f3fb14dc45bc8de55f8ab688b053ba10e6592
DinerIsmail <ismail.diner@gmail.com>
2021-04-21 00:22:03 +0100

    Test og tag in layout container

--- components/layout-container/LayoutContainer.jsx
+++ components/layout-container/LayoutContainer.jsx
@@ -1,11 +1,21 @@
 import { Box, SlideFade, useBreakpointValue } from '@chakra-ui/react';
+import Head from 'next/head';
 
 import Nav from '@components/nav';
 import Footer from '@components/footer';
 
-const LayoutContainer = ({ children }) => {
+const LayoutContainer = ({ children, title }) => {
 	return (
 		<div>
+			<Head>
+				{/* <title>{title}</title> */}
+				{/* <meta property="og:title" content={title} /> */}
+				<meta
+					property="og:description"
+					name="og:description"
+					content="Test description"
+				/>
+			</Head>
 			<Nav />
 			<SlideFade in>
 				<Box
